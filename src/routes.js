f784a709dd595f7c19b7bd31caa420be60c6e0ef
Emerson Silva <emerson_jdss@hotmail.com>
2019-08-14 18:40:31 -0300

    config bottoms Tabs

--- src/routes.js
+++ src/routes.js
@@ -2,20 +2,44 @@ import {
   createAppContainer,
   createSwitchNavigator,
   createStackNavigator,
-  createBottomTabNavigator,
-} from 'react-navigation';
+  createBottomTabNavigator
+} from "react-navigation";
 
-import Login from "./pages/Login";
-import Home from './pages/Home';
+import Login from './pages/Login';
+import Home from "./pages/Home";
+import HomeList from "./pages/Tabs/HomeList";
+import Machines from "./pages/Tabs/Machines";
+import Calendar from "./pages/Tabs/Calendar";
 
 const BottomTabs = createBottomTabNavigator({
-  Home,
-}) ;
+  HomeList: {
+    screen: HomeList,
+    navigationOptions: {
+      title: 'Home',
+      tabBarLabel: 'HOME',
+    }
+  },
+  Machines: {
+    screen: Machines,
+    navigationOptions: {
+      title: 'Machines',
+      tabBarLabel: 'MACHINES',
+    }
+  },
+  Calendar: {
+    screen: Calendar,
+    navigationOptions: {
+      title: 'Calendar',
+      tabBarLabel: 'CALENDAR',
+    }
+  }
+});
 
 const Routes = createAppContainer(
   createSwitchNavigator({
+    BottomTabs,
     Login,
-    Home,
+    Home
   })
 );
 
